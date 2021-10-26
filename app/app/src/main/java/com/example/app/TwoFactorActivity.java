74af0111148597870234362d15bdfd745c4ea05c
Rodrigo <rodrigodimare1996@gmail.com>
2021-10-26 20:28:03 -0300

    Agrega autenticacion y logeo a home desde activity de registro

--- app/app/src/main/java/com/example/app/TwoFactorActivity.java
+++ app/app/src/main/java/com/example/app/TwoFactorActivity.java
@@ -2,12 +2,20 @@ package com.example.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.app.utils.Configuration.generateRandomCode;
+import com.example.app.api.RetrofitClient;
+import com.example.app.services.EmailService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TwoFactorActivity extends AppCompatActivity {
 
@@ -15,8 +23,13 @@ public class TwoFactorActivity extends AppCompatActivity {
     Button buttonReenviar;
     EditText txtAuth;
 
+    String randomCode = "";
+
+    EmailService _mailService = new EmailService();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twofactor);
 
@@ -27,23 +40,44 @@ public class TwoFactorActivity extends AppCompatActivity {
 
         Intent i = getIntent();
 
-        // Parémetros que recibo de la activity login/register //
+        /** Parémetros que recibo de la activity login/register **/
         String email = i.getStringExtra("email");
         String token = i.getStringExtra("token");
         String tokenRefresh = i.getStringExtra("tokenRefresh");
+        randomCode = i.getStringExtra("tokenAccess");
 
-        String randomCode = sendEmail(email,token);
+
+        buttonReenviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                randomCode = _mailService.sendEmail(email, TwoFactorActivity.this);
+
+                Toast.makeText(TwoFactorActivity.this, "Se ha enviado un nuevo token", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        buttonAuth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String tokenAuth = txtAuth.getText().toString();
+
+                if(tokenAuth.equals(randomCode)) {
+                    Intent intent = new Intent(TwoFactorActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(TwoFactorActivity.this, "Código de autenticación erroneo", Toast.LENGTH_SHORT).show();
+                    txtAuth.setText("");
+                }
+
+            }
+        });
+
 
     }
 
-    public String sendEmail(String email, String token){
-
-        Intent sendEmail = new Intent(Intent.ACTION_SEND);
-
-        String randomCode = generateRandomCode();
-
-        //EmailService emailService = new EmailService( this, email, "Token Autenticacion", token);
-
-        return randomCode;
-    }
 }
