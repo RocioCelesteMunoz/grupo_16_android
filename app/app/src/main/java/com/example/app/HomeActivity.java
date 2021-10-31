0b51884259c1919005427c433968be6e7b43cdca
Rocio Celeste Muñoz <rocio.c.munoz@outlook.com>
2021-10-31 17:36:01 -0300

    mostrar estado de bateria en Home

--- app/app/src/main/java/com/example/app/HomeActivity.java
+++ app/app/src/main/java/com/example/app/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +15,17 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        checkBattery();
+    }
+
+    private void checkBattery() {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent state = getBaseContext().registerReceiver(null, intentFilter);
+        int level = state.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = state.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        int percentage = level * 100 / scale;
+
+        Toast.makeText(this, "El nivel de bateria es del " + percentage + "%.", Toast.LENGTH_SHORT).show();
     }
 }
