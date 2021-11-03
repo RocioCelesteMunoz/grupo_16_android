ba0d120768a54c2bb6fa0405d7c8b177bd02cfe1
Rocio Celeste Muñoz <rocio.c.munoz@outlook.com>
2021-11-03 08:46:40 -0300

    Shake para reenviar mail

--- app/app/src/main/java/com/example/app/TwoFactorActivity.java
+++ app/app/src/main/java/com/example/app/TwoFactorActivity.java
@@ -1,6 +1,11 @@
 package com.example.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +17,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.app.services.EmailService;
 import com.example.app.utils.SessionManager;
 
+import java.text.DecimalFormat;
 
-public class TwoFactorActivity extends AppCompatActivity {
+
+public class TwoFactorActivity extends AppCompatActivity implements SensorEventListener {
+
+    private long lastUpdate = 0;
+    private float lastX, lastY, lastZ;
+    private static final int UMBRAL_SHAKE = 200;
 
     Button buttonAuth;
     Button buttonReenviar;
@@ -21,8 +32,12 @@ public class TwoFactorActivity extends AppCompatActivity {
 
     String randomCode = "";
 
+    String email;
+
     EmailService _mailService = new EmailService();
     SessionManager session;
+    private SensorManager mSensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -33,12 +48,14 @@ public class TwoFactorActivity extends AppCompatActivity {
         buttonReenviar = findViewById(R.id.buttonReenviar);
         txtAuth = findViewById(R.id.txtAuth);
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         session = new SessionManager(getApplicationContext());
         String tokenTes = session.getStringData("TOKEN");
         Intent i = getIntent();
 
         /** Parémetros que recibo de la activity login/register **/
-        String email = i.getStringExtra("email");
+        email = i.getStringExtra("email");
         String token = i.getStringExtra("token");
         String tokenRefresh = i.getStringExtra("tokenRefresh");
         randomCode = i.getStringExtra("tokenAccess");
@@ -75,6 +92,70 @@ public class TwoFactorActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    //uso sensor acelerometro para reenvio de mail
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String txt = "";
+
+
+        synchronized (this) {
+
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+                    long currentTime = System.currentTimeMillis();
+                    if ((currentTime - lastUpdate) > 200) {
+                        long diffTime = (currentTime - lastUpdate);
+                        lastUpdate = currentTime;
+                        float speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000;
+                        if (speed > UMBRAL_SHAKE) {
+                            randomCode = _mailService.sendEmail(email, TwoFactorActivity.this);
+                            Toast.makeText(TwoFactorActivity.this, "Se ha enviado un nuevo token", Toast.LENGTH_SHORT).show();
+                        }
+                        lastX = x;
+                        lastY = y;
+                        lastZ = z;
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onRestart() {
+        startSensors();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startSensors();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopSensors();
+    }
+
+    protected void startSensors() {
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void stopSensors() {
+        mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
     }
 
 }
